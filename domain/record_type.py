683e475607604fee4067ee3a046a14ce674319e6
fearppen <antonchachylin@gmail.com>
2021-04-26 17:07:09 +0500

    fix

--- domain/record_type.py
+++ domain/record_type.py
@@ -2,9 +2,10 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 
 from domain.db_session import SqlAlchemyBase
+from sqlalchemy_serializer import SerializerMixin
 
 
-class RecordType(SqlAlchemyBase):  # модель для работы с таблицей типов записей
+class RecordType(SqlAlchemyBase, SerializerMixin):  # модель для работы с таблицей типов записей
     __tablename__ = "record_types"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
